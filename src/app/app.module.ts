613d7ddba4ca04227480e595c95f01c84423ca54
CristianB13 <cristian36200119@gmail.com>
2022-12-11 01:40:04 +0200

    add reactive forms to tip-calculator component and validating input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TipCalculatorComponent } from './components/tip-calculator/tip-calculator.component';
@@ -10,7 +11,8 @@ import { TipCalculatorComponent } from './components/tip-calculator/tip-calculat
     TipCalculatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/tip-calculator/tip-calculator.component.ts
+++ src/app/components/tip-calculator/tip-calculator.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tip-calculator',
   templateUrl: './tip-calculator.component.html',
   styleUrls: ['./tip-calculator.component.scss']
 })
-export class TipCalculatorComponent {
+export class TipCalculatorComponent implements OnInit{
+  form!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      bill: ['', [
+        Validators.required,
+        Validators.min(0.00001),
+        Validators.pattern("[+-]?([0-9]*[.])?[0-9]+")
+      ]],
+      tip: ['', [
+        Validators.min(0.00001),
+        Validators.pattern("[+-]?([0-9]*[.])?[0-9]+")
+      ]],
+      nrOfPeople: ['', [
+        Validators.required,
+        Validators.min(0.00001),
+        Validators.pattern("^\\d+$")
+      ]]
+    })
+  }
+
+  get bill(): FormControl {
+    return this.form.get("bill") as FormControl;
+  }
+
+  get tip(): FormControl {
+    return this.form.get("tip") as FormControl;
+  }
+
+  get nrOfPeople(): FormControl {
+    return this.form.get("nrOfPeople") as FormControl;
+  }
 }
