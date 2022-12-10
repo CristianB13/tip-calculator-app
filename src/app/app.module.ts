c5213e93aa9da535b20cd272389af06b389b7cfb
CristianB13 <cristian36200119@gmail.com>
2022-12-10 23:36:46 +0200

    add _colors.scss file and generate tip-calculator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TipCalculatorComponent } from './components/tip-calculator/tip-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TipCalculatorComponent
   ],
   imports: [
     BrowserModule
