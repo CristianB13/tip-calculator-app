546a990a6b1b454036883e461b76b66ccff6880f
CristianB13 <cristian36200119@gmail.com>
2022-12-15 00:29:04 +0200

    add text-input component and update tip-calculator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TipCalculatorComponent } from './components/tip-calculator/tip-calculator.component';
 import { RadioButtonComponent } from './components/radio-button/radio-button.component';
+import { TextInputComponent } from './components/text-input/text-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TipCalculatorComponent,
-    RadioButtonComponent
+    RadioButtonComponent,
+    TextInputComponent
   ],
   imports: [
     BrowserModule,
